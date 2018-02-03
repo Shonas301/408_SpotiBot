732e92fb15c73d85e290872a775ffed195ac4b69
Ubuntu <ubuntu@ip-172-31-46-168.us-east-2.compute.internal>
2018-02-03 20:46:05 +0000

    adding ssl protection

--- apirequests.js
+++ apirequests.js
@@ -1,3 +1,20 @@
 //Just a file to keep track of what we know is written
 //insert below
 
+// Sign into FB messenger account
+
+// Sign into spotify
+
+// As a user, I want to be able to input a number of months and get a playlist of my top tracks from that time period
+
+// As a user, I want to be able to view what is the most common genre that I listen to
+
+// As a user, I want to be able to view the most common key of the music I listen to
+
+// As a user, I want to be able to see the happiest songs that I listen to
+
+// As a user, I want to be able to see the saddest songs that I listen to
+
+// As a user I want to be able to view the slowest songs that I listen to
+
+// As a user, I want to be able to view the fastest songs that I listen to
