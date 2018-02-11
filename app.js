7033611f172714127a30d7da1cfadaaa915953ba
Jason Shipp <shippj@purdue.edu>
2018-02-11 17:22:58 -0500

    test

--- app.js
+++ app.js
@@ -74,6 +74,18 @@ app.get('/clientAuth', (req, res) => {
   var code = req.query.code;
   console.log(req);
   spotifyApi.setAccessToken(code);
+  spotifyApi.authorizationCodeGrant(code)
+  .then(function(data) {
+    console.log('The token expires in ' + data.body['expires_in']);
+    console.log('The access token is ' + data.body['access_token']);
+    console.log('The refresh token is ' + data.body['refresh_token']);
+
+    // Set the access token on the API object to use it in later calls
+    spotifyApi.setAccessToken(data.body['access_token']);
+    spotifyApi.setRefreshToken(data.body['refresh_token']);
+  }, function(err) {
+    console.log('Something went wrong!', err);
+  });  
   spotifyApi.getMe()
     .then(function(data) {
       console.log('info about the user', data.body)
