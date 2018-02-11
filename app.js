395d2ed92f7c4aa24f5d0c95e3f8946fd6404b0b
Jason Shipp <shippj@purdue.edu>
2018-02-11 18:46:44 -0500

    updated page access

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
  */
 
 'use strict';
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const PAGE_ACCESS_TOKEN = encryption/pageaccess.token;
 
 // Imports dependencies and set up http server
 const
