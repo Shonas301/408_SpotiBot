b9acdc5907e8efa48badccc653c4102b8f9d00bc
Jason Shipp <shippj@purdue.edu>
2018-02-11 23:58:17 -0500

    maybe works

--- app.js
+++ app.js
@@ -204,8 +204,9 @@ function handleMessage(sender_psid, received_message) {
              songs.push(song)
             });
           console.log('bout to print some songs')
-          for(var i = 0; i < 50, i++)
+          for(var i = 0; i < 50, i++){
             songlist.push(songs[i].name)
+          }
         }).then(function() { 
           callSendAPI(sender_psid,songlist.toString());
          });
