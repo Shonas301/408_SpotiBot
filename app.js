4649630337aa6c0ceb2f58670c6970ae31bd7f6b
bonetail <worleyd@purdue.edu>
2018-02-17 18:24:11 -0500

    added pause to prevent race condition

--- app.js
+++ app.js
@@ -105,6 +105,7 @@ app.get('/clientAuth', (req, res) => {
       reject(err);
     }).then(function () {
             callSendAPI(sender_psid, response) // sends response text "Great! Thanks ..."
+            await sleep(500);
     }).then(function () {
         response = {
             "text": `
