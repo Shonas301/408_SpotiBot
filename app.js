54db73939701b68ac0b1aac24350bc787e93d50e
Jason Shipp <shippj@purdue.edu>
2018-03-04 22:37:19 -0500

    fixed bug on lost colon

--- app.js
+++ app.js
@@ -343,6 +343,10 @@ function handleMessage(sender_psid, received_message) {
     else if (received_message.text.toLowerCase().substring(0, 4) === "byop") {
 
     	var res = received_message.text.split(":")
+        if (res === undefined) {
+          var response {"text": "Hey, looks like you forgot the colon! The correct format is:\nbyop -type-: list, "}
+          callSendAPI(sender_psid, response)
+        }
         console.log(res)
     	var input = res[0].substring(5)
       switch(input) {
