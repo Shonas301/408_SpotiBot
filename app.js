f5c1b44cbff16724cf50e18f686c661b60569934
Jason Shipp <shippj@purdue.edu>
2018-03-18 16:45:54 -0700

    everything is wrapped

--- app.js
+++ app.js
@@ -916,113 +916,122 @@ function getHappiestSong(id) {
           throw err;
         });
       });
-    }
+    })
 }
 
 function getSaddestSong(id) {
-  return new Promise((resolve, reject) => {
-    spotifyApi.getMyTopTracks({
-      limit: 50
-    }).then(function (data) {
-      var songsList = []
-      var danceList = []
-      var songs = data.body.items
-      var saddest_song
-      for (var index = 0; index < songs.length; ++index) {
-        songsList.push(songs[index].id)
-      }
-      spotifyApi.getAudioFeaturesForTracks(songsList).then(function (data) {
-        var audio_features = data.body.audio_features
-        for (var index = 0; index < songs.length; ++index) {
-          var temp = []
-          temp.push(songsList[index])
-          temp.push(audio_features[index].valence)
-          danceList.push(temp)
-        }
-        danceList.sort(function (a, b) { return a[1] - b[1] });
-      }, function (err) {
-        console.log(err)
-      }).then(function () {
-        return resolve(danceList[0][0])
-      })
-    }).catch(function (err) {
-      console.error(err)
-    });
-  });
+  refreshID(id)
+    .then(() => {
+      return new Promise((resolve, reject) => {
+        spotifyApi.getMyTopTracks({
+          limit: 50
+        }).then(function (data) {
+          var songsList = []
+          var danceList = []
+          var songs = data.body.items
+          var saddest_song
+          for (var index = 0; index < songs.length; ++index) {
+            songsList.push(songs[index].id)
+          }
+          spotifyApi.getAudioFeaturesForTracks(songsList).then(function (data) {
+            var audio_features = data.body.audio_features
+            for (var index = 0; index < songs.length; ++index) {
+              var temp = []
+              temp.push(songsList[index])
+              temp.push(audio_features[index].valence)
+              danceList.push(temp)
+            }
+            danceList.sort(function (a, b) { return a[1] - b[1] });
+          }, function (err) {
+            console.log(err)
+          }).then(function () {
+            return resolve(danceList[0][0])
+          })
+        }).catch(function (err) {
+          console.error(err)
+        });
+      });
+    })
 }
 //finish
 function getFastestSong(id) {
-  return new Promise((resolve, reject) => {
-    spotifyApi.getMyTopTracks({
-      limit: 50
-    }).then(function (data) {
-      var songsList = []
-      var danceList = []
-      var songs = data.body.items
-      var fastest_song
-      for (var index = 0; index < songs.length; ++index) {
-        songsList.push(songs[index].id)
-      }
-      spotifyApi.getAudioFeaturesForTracks(songsList).then(function (data) {
-        var audio_features = data.body.audio_features
-        for (var index = 0; index < songs.length; ++index) {
-          var temp = []
-          temp.push(songsList[index])
-          temp.push(audio_features[index].energy)
-          danceList.push(temp)
-        }
-        danceList.sort(function (a, b) { return b[1] - a[1] });
-      }, function (err) {
-        console.log(err)
-      }).then(function () {
-        return resolve(danceList[0][0])
-      })
-    }).catch(function (err) {
-      console.error(err)
-    });
-  });
+  refreshID(id)
+    .then(() => {
+      return new Promise((resolve, reject) => {
+        spotifyApi.getMyTopTracks({
+          limit: 50
+        }).then(function (data) {
+          var songsList = []
+          var danceList = []
+          var songs = data.body.items
+          var fastest_song
+          for (var index = 0; index < songs.length; ++index) {
+            songsList.push(songs[index].id)
+          }
+          spotifyApi.getAudioFeaturesForTracks(songsList).then(function (data) {
+            var audio_features = data.body.audio_features
+            for (var index = 0; index < songs.length; ++index) {
+              var temp = []
+              temp.push(songsList[index])
+              temp.push(audio_features[index].energy)
+              danceList.push(temp)
+            }
+            danceList.sort(function (a, b) { return b[1] - a[1] });
+          }, function (err) {
+            console.log(err)
+          }).then(function () {
+            return resolve(danceList[0][0])
+          })
+        }).catch(function (err) {
+          console.error(err)
+        });
+      });
+    })
 }
 
 function getSlowestSong(id) {
-  return new Promise((resolve, reject) => {
-    spotifyApi.getMyTopTracks({
-      limit: 50
-    }).then(function (data) {
-      var songsList = []
-      var danceList = []
-      var songs = data.body.items
-      var slowest_song
-      for (var index = 0; index < songs.length; ++index) {
-        songsList.push(songs[index].id)
-      }
-      spotifyApi.getAudioFeaturesForTracks(songsList).then(function (data) {
-        var audio_features = data.body.audio_features
-        for (var index = 0; index < songs.length; ++index) {
-          var temp = []
-          temp.push(songsList[index])
-          temp.push(audio_features[index].energy)
-          danceList.push(temp)
-        }
-        danceList.sort(function (a, b) { return a[1] - b[1] });
-        spotifyApi.getAudioFeaturesForTracks(songsList).then(function (data) {
-          var audio_features = data.body.audio_features
+  refreshID(id)
+    .then(() => {
+      return new Promise((resolve, reject) => {
+        spotifyApi.getMyTopTracks({
+          limit: 50
+        }).then(function (data) {
+          var songsList = []
+          var danceList = []
+          var songs = data.body.items
+          var slowest_song
           for (var index = 0; index < songs.length; ++index) {
-            var temp = []
-            temp.push(songsList[index])
-            temp.push(audio_features[index].energy)
-            danceList.push(temp)
+            songsList.push(songs[index].id)
           }
-          danceList.sort(function (a, b) { return a[1] - b[1] });
-        }, function (err) {
-          console.log(err)
-        }).then(function () {
-          return resolve(danceList[0][0])
-        })
-      }).catch(function (err) {
-        console.error(err)
+          spotifyApi.getAudioFeaturesForTracks(songsList).then(function (data) {
+            var audio_features = data.body.audio_features
+            for (var index = 0; index < songs.length; ++index) {
+              var temp = []
+              temp.push(songsList[index])
+              temp.push(audio_features[index].energy)
+              danceList.push(temp)
+            }
+            danceList.sort(function (a, b) { return a[1] - b[1] });
+            spotifyApi.getAudioFeaturesForTracks(songsList).then(function (data) {
+              var audio_features = data.body.audio_features
+              for (var index = 0; index < songs.length; ++index) {
+                var temp = []
+                temp.push(songsList[index])
+                temp.push(audio_features[index].energy)
+                danceList.push(temp)
+              }
+              danceList.sort(function (a, b) { return a[1] - b[1] });
+            }, function (err) {
+              console.log(err)
+            }).then(function () {
+              return resolve(danceList[0][0])
+            })
+          }).catch(function (err) {
+            console.error(err)
+          });
+        });
       });
-    });
-  });
+    })
 }
 
 function createPlaylistForCategory(id, categories, count) {
@@ -1032,35 +1041,41 @@ function createPlaylistForCategory(id, categories, count) {
     console.log(cat)
   });
 
-  return Promise.all(categories.map(function (category) {
-    return new Promise((resolve, reject) => {
-      spotifyApi.getPlaylistsForCategory(category).then((res) => {
-        var playlists = [];
-        for (var i = 0; i < count; i++)
-          playlists.push({ "link": res.body.playlists.items[i].external_urls.spotify, "name": res.body.playlists.items[i].name })
-        console.log(playlists)
-        resolve(playlists);
-      });
-    }).catch(err => {
-      reject(err);
+  refreshID(id)
+    .then(() => {
+      return Promise.all(categories.map(function (category) {
+        return new Promise((resolve, reject) => {
+          spotifyApi.getPlaylistsForCategory(category).then((res) => {
+            var playlists = [];
+            for (var i = 0; i < count; i++)
+              playlists.push({ "link": res.body.playlists.items[i].external_urls.spotify, "name": res.body.playlists.items[i].name })
+            console.log(playlists)
+            resolve(playlists);
+          });
+        }).catch(err => {
+          reject(err);
+        })
+      }))
     })
-  }))
 }
 
 
 //************************** SONG PLAYLIST
 function searchForSongByArtist(id, query) {
-  return new Promise((resolve, reject) => {
-    var result
-    spotifyApi.searchTracks(query).then(function (data) {
-      result = data.body.tracks.items[0].id;
-      console.log("here: " + result)
-    }, function (err) {
-      console.log(err)
-    }).then(function () {
-      return resolve(result)
+  refreshID(id)
+    .then( () => {
+      return new Promise((resolve, reject) => {
+        var result
+        spotifyApi.searchTracks(query).then(function (data) {
+          result = data.body.tracks.items[0].id;
+          console.log("here: " + result)
+        }, function (err) {
+          console.log(err)
+        }).then(function () {
+          return resolve(result)
+        })
+      });
     })
-  });
 }
 
 //takes in an array of song names from user and returns an array of song IDs
@@ -1071,7 +1086,7 @@ function getSongIDArray(id, song_array) {
   var song_id_array = []
   return new Promise((resolve, reject) => {
     for (var index = 0; index < song_array.length; ++index) {
-      searchForSongByArtist(song_array[index]).then(function (data) {
+      searchForSongByArtist(id, song_array[index]).then(function (data) {
         song_id_array.push(data)
         resolve(song_id_array);
       })
@@ -1085,15 +1100,18 @@ function getSongIDArray(id, song_array) {
 //takes in array of song id's and returns and object that is a list of tracks in recommended playlist
 //TODO list could be converted to song uri's to work with creating a playlist that we have in app.js
 function getPlaylist(id, tracks) {
-  return new Promise((resolve, reject) => {
-    spotifyApi.getRecommendations({
-      min_energy: 0.4,
-      seed_tracks: tracks,
-      min_popularity: 50
-    }).then(function (data) {
-      return resolve(data.body.tracks)
+  refreshID(id)
+    .then(() => {
+      return new Promise((resolve, reject) => {
+        spotifyApi.getRecommendations({
+          min_energy: 0.4,
+          seed_tracks: tracks,
+          min_popularity: 50
+        }).then(function (data) {
+          return resolve(data.body.tracks)
+        })
+      });
     })
-  });
 }
 
 //function takes in a string from user like this 'Just Dance - Lady Gaga, Strange Love - Halsey'
@@ -1104,7 +1122,7 @@ function playlistFromSongs(id, input) {
     var song_array = input.split(',');
     getSongIDArray(song_array).then(function (song_id_array) {
       console.log('song array is: ' + song_id_array)
-      getPlaylist(song_id_array).then(function (playlist) {
+      getPlaylist(id, song_id_array).then(function (playlist) {
         return resolve(playlist);
       })
     }).catch(function (err) {
@@ -1115,41 +1133,44 @@ function playlistFromSongs(id, input) {
 
 // This function combines everything and generates a playlist, and returns a promise contiaining the url
 function buildSongPlaylist(id, songs) {
-  return new Promise((resolve, reject) => {
-    playlistFromSongs(songs).then(function (tracks) {
-      var song_ids = [];
-      for (var i = 0; i < tracks.length; i++) {
-        song_ids.push("spotify:track:" + tracks[i].id)
-      }
-      var getMe = spotifyApi.getMe()
-        .then(function (data) {
-          return data.body.id;
-        }).catch(function (err) {
-          throw err;
-        })
+  refreshID(id)
+    .then(() => {
+      return new Promise((resolve, reject) => {
+        playlistFromSongs(songs).then(function (tracks) {
+          var song_ids = [];
+          for (var i = 0; i < tracks.length; i++) {
+            song_ids.push("spotify:track:" + tracks[i].id)
+          }
+          var getMe = spotifyApi.getMe()
+            .then(function (data) {
+              return data.body.id;
+            }).catch(function (err) {
+              throw err;
+            })
 
-      // Create a playlist using the user's id
-      getMe.then(function (user_id) {
-        // Create a public playlist
-        return spotifyApi.createPlaylist(user_id, songs, { 'public': true })
-          .then(function (data) {
-            // Return array of playlist_id and user_id
-            return [data.body.id, user_id, data.body.external_urls.spotify];
-          }).catch(function (err) {
-            throw err;
-          });
-      }).then((result) => {
-        spotifyApi.addTracksToPlaylist(result[1], result[0], song_ids)
-          .then((res) => {
-            resolve(result[2])
+          // Create a playlist using the user's id
+          getMe.then(function (user_id) {
+            // Create a public playlist
+            return spotifyApi.createPlaylist(user_id, songs, { 'public': true })
+              .then(function (data) {
+                // Return array of playlist_id and user_id
+                return [data.body.id, user_id, data.body.external_urls.spotify];
+              }).catch(function (err) {
+                throw err;
+              });
+          }).then((result) => {
+            spotifyApi.addTracksToPlaylist(result[1], result[0], song_ids)
+              .then((res) => {
+                resolve(result[2])
+              }).catch((err) => {
+                reject(err);
+              })
           }).catch((err) => {
             reject(err);
           })
-      }).catch((err) => {
-        reject(err);
+        })
       })
     })
-  })
 }
 
 // **************** ARTIST PLAYLIST
@@ -1157,18 +1178,21 @@ function buildSongPlaylist(id, songs) {
 // takes a name of artist in the format 'Hayley Kiyoko' and searches for the artist
 //returns the first result's artist id (should be accurate almost all the time)
 function searchForArtist(id, query) {
-  return new Promise((resolve, reject) => {
-    var result
-    spotifyApi.searchArtists(query).then(function (data) {
-      //console.log(data)
-      result = data.body.artists.items[0].id;
-      console.log("here: " + result)
-    }, function (err) {
-      console.log(err)
-    }).then(function () {
-      return resolve(result)
+  refreshID(id)
+    .then(() => {
+      return new Promise((resolve, reject) => {
+        var result
+        spotifyApi.searchArtists(query).then(function (data) {
+          //console.log(data)
+          result = data.body.artists.items[0].id;
+          console.log("here: " + result)
+        }, function (err) {
+          console.log(err)
+        }).then(function () {
+          return resolve(result)
+        })
+      });
     })
-  });
 }
 
 
@@ -1176,14 +1200,14 @@ function searchForArtist(id, query) {
 //PROBLEM: I can't get it to return a list of all the song ID's 
 //right now it adds the first artist ID to the array, returns the array and then gets the IDS for the rest of the artists
 function getArtistIDArray(id, artist_array) {
-  var artist_id_array = []
-  return new Promise((resolve, reject) => {
+  var artist_id_array = new Promise((resolve, reject) => {
+    var temp = []
     for (var index = 0; index < artist_array.length; ++index) {
-      searchForArtist(artist_array[index]).then(function (data) {
-        artist_id_array.push(data)
-        resolve(artist_id_array);
+      searchForArtist(id, artist_array[index]).then(function (data) {
+        temp.push(data)
       })
     }
+    resolve(temp)
   });
   return artist_id_array;
 }
@@ -1192,15 +1216,18 @@ function getArtistIDArray(id, artist_array) {
 //takes in array of artist id's and returns and object that is a list of tracks in recommended playlist
 //TODO list could be converted to song uri's to work with creating a playlist that we have in app.js
 function getPlaylist(id, artists) {
-  return new Promise((resolve, reject) => {
-    spotifyApi.getRecommendations({
-      min_energy: 0.4,
-      seed_artists: artists,
-      min_popularity: 50
-    }).then(function (data) {
-      return resolve(data.body.tracks)
+  refreshID(id)
+    .then(() => {
+      return new Promise((resolve, reject) => {
+        spotifyApi.getRecommendations({
+          min_energy: 0.4,
+          seed_artists: artists,
+          min_popularity: 50
+        }).then(function (data) {
+          return resolve(data.body.tracks)
+        })
+      });
     })
-  });
 }
 
 
