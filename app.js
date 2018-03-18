682e0fa106891344a25a9e1fb0586b3f057aa09f
carlso70 <carlso70@purdue.edu>
2018-03-18 15:13:42 -0500

    Added song/artist playlist

--- app.js
+++ app.js
@@ -393,10 +393,30 @@ function handleMessage(sender_psid, received_message) {
             for (var i = 0; i < songs_list.length; i++) {
               songs_string = songs_string + songs_list[i] + ' '
             }
-            var response = { "text": `Your songs are: ${songs_string}\n` }
+            //var response = { "text": `Your songs are: ${songs_string}\n` }
+            buildSongPlaylist(songs_string).then(res => {
+              var response = { "text": `Your playlist:\n${res}\n` }
+              callSendAPI(sender_psid, response);
+            }).catch(err => {
+              var response = { "text": `Sorry there was an error: ${err.message}\n` }
+              callSendAPI(sender_psid, response);
+            })
 
-            callSendAPI(sender_psid, response);
           } else if (input == 'byop artist') {
+            var artist_list = res[1].split(",")
+            var artist_string = ''
+            for (var i = 0; i < songs_list.length; i++) {
+              artist_string = artist_string + artist_list[i] + ' '
+            }
+
+            buildArtistPlaylists(artist_string).then(res => {
+              var response = { "text": `Your playlist:\n${res}\n` }
+              callSendAPI(sender_psid, response);
+            }).catch(err => {
+              var response = { "text": `Sorry there was an error: ${err.message}\n` }
+              callSendAPI(sender_psid, response);
+            })
+
           } else if (input == 'byop playlist') {
           } else if (input == 'byop genre') {
             var genres_list = res[1].split(" ")
@@ -952,7 +972,7 @@ function getSlowestSong(id) {
 function createPlaylistForCategory(categories, count) {
   console.log("Categories = ")
 
-  categories.map(function(cat) {
+  categories.map(function (cat) {
     console.log(cat)
   });
 
@@ -969,4 +989,218 @@ function createPlaylistForCategory(categories, count) {
       reject(err);
     })
   }))
+}
+
+
+//************************** SONG PLAYLIST
+function searchForSongByArtist(query) {
+  return new Promise((resolve, reject) => {
+    var result
+    spotifyApi.searchTracks(query).then(function (data) {
+      result = data.body.tracks.items[0].id;
+      console.log("here: " + result)
+    }, function (err) {
+      console.log(err)
+    }).then(function () {
+      return resolve(result)
+    })
+  });
+}
+
+//takes in an array of song names from user and returns an array of song IDs
+//PROBLEM: I can't get it to return a list of all the song ID's 
+//right now it adds the first song ID to the array, returns the array and then gets the IDS for the rest of the songs
+function getSongIDArray(song_array) {
+  console.log(typeof song_array)
+  var song_id_array = []
+  return new Promise((resolve, reject) => {
+    for (var index = 0; index < song_array.length; ++index) {
+      searchForSongByArtist(song_array[index]).then(function (data) {
+        song_id_array.push(data)
+        resolve(song_id_array);
+      })
+    }
+  });
+  console.log(typeof song_id_array)
+  return song_id_array;
+}
+
+
+//takes in array of song id's and returns and object that is a list of tracks in recommended playlist
+//TODO list could be converted to song uri's to work with creating a playlist that we have in app.js
+function getPlaylist(tracks) {
+  return new Promise((resolve, reject) => {
+    spotifyApi.getRecommendations({
+      min_energy: 0.4,
+      seed_tracks: tracks,
+      min_popularity: 50
+    }).then(function (data) {
+      return resolve(data.body.tracks)
+    })
+  });
+}
+
+//function takes in a string from user like this 'Just Dance - Lady Gaga, Strange Love - Halsey'
+//returns a playlist object
+//TODO or here list could be converted to song uri's to work with creating a playlist that we have in app.js
+function playlistFromSongs(input) {
+  return new Promise((resolve, reject) => {
+    var song_array = input.split(',');
+    getSongIDArray(song_array).then(function (song_id_array) {
+      console.log('song array is: ' + song_id_array)
+      getPlaylist(song_id_array).then(function (playlist) {
+        return resolve(playlist);
+      })
+    }).catch(function (err) {
+      console.error(err)
+    });
+  });
+}
+
+// This function combines everything and generates a playlist, and returns a promise contiaining the url
+function buildSongPlaylist(songs) {
+  return new Promise((resolve, reject) => {
+    playlistFromSongs(songs).then(function (tracks) {
+      var song_ids = [];
+      for (var i = 0; i < tracks.length; i++) {
+        song_ids.push("spotify:track:" + tracks[i].id)
+      }
+      var getMe = spotifyApi.getMe()
+        .then(function (data) {
+          return data.body.id;
+        }).catch(function (err) {
+          throw err;
+        })
+
+      // Create a playlist using the user's id
+      getMe.then(function (user_id) {
+        // Create a public playlist
+        return spotifyApi.createPlaylist(user_id, songs, { 'public': true })
+          .then(function (data) {
+            // Return array of playlist_id and user_id
+            return [data.body.id, user_id, data.body.external_urls.spotify];
+          }).catch(function (err) {
+            throw err;
+          });
+      }).then((result) => {
+        spotifyApi.addTracksToPlaylist(result[1], result[0], song_ids)
+          .then((res) => {
+            resolve(result[2])
+          }).catch((err) => {
+            reject(err);
+          })
+      }).catch((err) => {
+        reject(err);
+      })
+    })
+  })
+}
+
+// **************** ARTIST PLAYLIST
+
+// takes a name of artist in the format 'Hayley Kiyoko' and searches for the artist
+//returns the first result's artist id (should be accurate almost all the time)
+function searchForArtist(query) {
+  return new Promise((resolve, reject) => {
+    var result
+    spotifyApi.searchArtists(query).then(function (data) {
+      //console.log(data)
+      result = data.body.artists.items[0].id;
+      console.log("here: " + result)
+    }, function (err) {
+      console.log(err)
+    }).then(function () {
+      return resolve(result)
+    })
+  });
+}
+
+
+//takes in an array of artist names from user and returns an array of artist IDs
+//PROBLEM: I can't get it to return a list of all the song ID's 
+//right now it adds the first artist ID to the array, returns the array and then gets the IDS for the rest of the artists
+function getArtistIDArray(artist_array) {
+  var artist_id_array = []
+  return new Promise((resolve, reject) => {
+    for (var index = 0; index < artist_array.length; ++index) {
+      searchForArtist(artist_array[index]).then(function (data) {
+        artist_id_array.push(data)
+        resolve(artist_id_array);
+      })
+    }
+  });
+  return artist_id_array;
+}
+
+
+//takes in array of artist id's and returns and object that is a list of tracks in recommended playlist
+//TODO list could be converted to song uri's to work with creating a playlist that we have in app.js
+function getPlaylist(artists) {
+  return new Promise((resolve, reject) => {
+    spotifyApi.getRecommendations({
+      min_energy: 0.4,
+      seed_artists: artists,
+      min_popularity: 50
+    }).then(function (data) {
+      return resolve(data.body.tracks)
+    })
+  });
+}
+
+
+//function takes in a string from user like this 'Lady Gaga, Halsey'
+//returns a playlist object
+//TODO or here list could be converted to song uri's to work with creating a playlist that we have in app.js
+function playlistFromArtists(input) {
+  return new Promise((resolve, reject) => {
+    var artist_array = input.split(',');
+    getArtistIDArray(artist_array).then(function (artist_id_array) {
+      console.log('artist array is: ' + artist_id_array)
+      getPlaylist(artist_id_array).then(function (playlist) {
+        return resolve(playlist);
+      })
+    }).catch(function (err) {
+      console.error(err)
+    });
+  });
+}
+
+// This function combines everything and generates a playlist, and returns a promise containing the playlist url
+function buildArtistPlaylists(artists) {
+  return new Promise((resolve, reject) => {
+    playlistFromArtists(artists).then(function (tracks) {
+      var song_ids = [];
+      for (var i = 0; i < tracks.length; i++) {
+        song_ids.push("spotify:track:" + tracks[i].id)
+      }
+      var getMe = spotifyApi.getMe()
+        .then(function (data) {
+          return data.body.id;
+        }).catch(function (err) {
+          throw err;
+        })
+
+      // Create a playlist using the user's id
+      getMe.then(function (user_id) {
+        // Create a public playlist
+        return spotifyApi.createPlaylist(user_id, artists, { 'public': true })
+          .then(function (data) {
+            // Return array of playlist_id and user_id
+            return [data.body.id, user_id, data.body.external_urls.spotify];
+          }).catch(function (err) {
+            throw err;
+          });
+      }).then((result) => {
+        console.log(result)
+        spotifyApi.addTracksToPlaylist(result[1], result[0], song_ids)
+          .then((res) => {
+            resolve(result[2])
+          }).catch((err) => {
+            reject(err);
+          })
+      }).catch((err) => {
+        reject(err);
+      })
+    })
+  })
 }
